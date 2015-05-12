92b995b8317acca7c8cca8f8d2faed66dd0cbdc7
Edwin Lunandy <populationtwo@gmail.com>
2015-05-12 11:47:59 -0400

    First pass at directive.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,33 @@
-angular.module('calendarDemoApp', []);
+angular.module( 'calendarApp', [] )
+	.directive( 'calendar', function () {
+		return {
+			restrict   : 'E',
+			templateUrl: 'calendar-template.html',
+			controller : 'calendarCtrl'
+		}
+	} )
+	.controller( 'calendarCtrl', function ($scope, $element, $attrs) {
+
+		var date = new Date();
+		var range1 = CalendarRange.getMonthlyRange(new Date());
+		console.log(range1.first);
+		console.log(range1.start);
+		console.log(range1.end);
+		console.log(range1.last);
+		console.log(range1.days);
+
+
+		$scope.range = CalendarRange.getMonthlyRange(new Date());
+		$scope.range.days.forEach(changeDayClass);
+
+		function changeDayClass(element){
+			if ( element.month < date.getMonth() || element.month > date.getMonth()) {
+				element.dayClass = 'outside';
+			}
+
+		}
+
+
+	} )
 
 // your controller and directive code go here
